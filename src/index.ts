7295e38b7686f99b780558a4d5551dc149d6c24b
Lagunita <lagunamichael2@outlook.com>
2020-07-20 18:13:01 -0600

    fix port

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 
 // port is now available to the Node.js runtime
 // as if it were an environment variable
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT || 5000
 const dburl = "mongodb+srv://admin:oQPCCoaopX7vkdEo@cluster0.t5rl5.mongodb.net/MongoLABDB?retryWrites=true&w=majority";
 
 const app = express();
