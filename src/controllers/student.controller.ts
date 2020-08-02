8a7b81cb5d9e4596ba97fd2329ce4a8672e3be77
monzork <malonzoareas@gmail.com>
2020-08-01 21:36:43 -0600

    students test with req

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -1,9 +1,8 @@
-import { IStudents } from "../models/student";
+import Student, { IStudents } from "../models/student";
 import { Response } from "express";
-import student from "../models/student";
 
-function getStudents(res: Response) {
-  student.find({}).then((docs: IStudents[]) => {
+function getStudents(req: Request, res: Response) {
+  Student.find({}).then((docs: IStudents[]) => {
     res.status(200).json(docs);
   });
 }
