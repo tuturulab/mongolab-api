ab17d4e2113cf7332b03be3f3d09fe015c636572
monzork <malonzoareas@gmail.com>
2020-08-01 21:06:07 -0600

    students router created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import customers from './customer.route';
+import express from "express";
+import customers from "./customer.route";
+import students from "./students.route";
 
 const router = express.Router();
 
-router.use('/customers', customers);
+router.use("/customers", customers);
+router.use("/students", students);
 
 export default router;
--- src/routes/students.route.ts
+++ src/routes/students.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import student from "../controllers/student";
+import student from "../controllers/student.controller";
 
 const router = express.Router();
 
